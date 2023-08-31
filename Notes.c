2430752cbcd7db008fd3c5ed124122aadc6a1aea
abdalla <abdallaskar2000@gmail.com>
2023-08-31 16:35:36 +0300

    add day2 and and add one a new notes

--- Notes.c
+++ Notes.c
@@ -4,3 +4,10 @@
 printf("%.6f\n", num);
 2- print long value 
 printf("%ld\n", longvalue);
+
+3- for using file in c 
+
+	FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");  // open file in write mode 
+	reuslt = 120;
+    fprintf(fptr, "%d\n", result);	// for write in feile 
+    fclose(fptr);					// then close file 
